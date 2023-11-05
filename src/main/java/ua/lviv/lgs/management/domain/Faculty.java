8ecc8cd68e18c4adc6861daeb516c4c1037b2cbe
olegnik <olegnik11@ukr.net>
2023-11-05 22:51:16 +0200

    Add domain

---
+++ src/main/java/ua/lviv/lgs/management/domain/Faculty.java
@@ -0,0 +1,91 @@
+package ua.lviv.lgs.management.domain;
+
+public class Faculty {
+
+	private Integer id;
+	private String name;
+	private Integer students;
+
+	public Faculty() {
+
+	}
+
+	public Faculty(String name, Integer students) {
+		this.name = name;
+		this.students = students;
+	}
+
+	public Faculty(Integer id, String name, Integer students) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.students = students;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getStudents() {
+		return students;
+	}
+
+	public void setStudents(Integer students) {
+		this.students = students;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((students == null) ? 0 : students.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Faculty other = (Faculty) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (students == null) {
+			if (other.students != null)
+				return false;
+		} else if (!students.equals(other.students))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Faculty [id=" + id + ", name=" + name + ", students=" + students + "]";
+	}
+
+}
---
+++ src/main/java/ua/lviv/lgs/management/domain/Subjects.java
@@ -0,0 +1,5 @@
+package ua.lviv.lgs.management.domain;
+
+public enum Subjects {
+	HISTORY, UKRAINE, ENGLISH, MATHEMATICS, GEOGRAPHY, PHISICS, CHEMISTRY, COMPUTER_SCIENCE, SPORTS, CERTIFICATE_MARK
+}
