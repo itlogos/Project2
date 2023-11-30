cda3be474f7f90e7f8db0d67f366dec25bfec489
olegnik <olegnik11@ukr.net>
2023-11-30 21:00:52 +0200

    Add logging

--- src/main/java/ua/lviv/lgs/management/service/FacultyService.java
+++ src/main/java/ua/lviv/lgs/management/service/FacultyService.java
@@ -2,27 +2,34 @@ package ua.lviv.lgs.management.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import ua.lviv.lgs.management.service.FacultyService;
 import ua.lviv.lgs.management.dao.FacultyRepository;
 import ua.lviv.lgs.management.domain.Faculty;
 
 @Service
 public class FacultyService {
+	private Logger logger = LoggerFactory.getLogger(FacultyService.class);
 
 	@Autowired
 	private FacultyRepository facultyRepository;
 
 	public Faculty save(Faculty faculty) {
+		logger.info("Save faculty {} - " + faculty);
 		return facultyRepository.save(faculty);
 	}
 
 	public List<Faculty> findAllFaculties() {
+		logger.info("Find all faculties");
 		return facultyRepository.findAll();
 	}
 	
 	public Faculty findFacultyById(Integer id) {
+		logger.info("Find faculty by id - " + id);
 		return facultyRepository.findById(id).get();
 	}
 
--- src/main/java/ua/lviv/lgs/management/service/StatementService.java
+++ src/main/java/ua/lviv/lgs/management/service/StatementService.java
@@ -2,23 +2,29 @@ package ua.lviv.lgs.management.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import ua.lviv.lgs.management.service.StatementService;
 import ua.lviv.lgs.management.dao.StatementRepository;
 import ua.lviv.lgs.management.domain.Statement;
 
 @Service
 public class StatementService {
-
+	private Logger logger = LoggerFactory.getLogger(StatementService.class);
+	
 	@Autowired
 	private StatementRepository statementRepository;
 
 	public void save(Statement statement) {
+		logger.info("Save statement {} - " + statement);
 		statementRepository.save(statement);
 	}
 
 	public List<Statement> findAllStatements() {
+		logger.info("Find all registered statement");
 		return statementRepository.findAll();
 	}
 }
